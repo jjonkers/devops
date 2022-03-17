39315835d72c35461086ff2ed762a97f9bc3e00d
jjonkers <47516121+jjonkers@users.noreply.github.com>
2022-03-17 07:11:05 +0100

    Add files via upload

--- disablenetwork.ps1
+++ disablenetwork.ps1
@@ -1,3 +1,7 @@
 $name = Get-NetIPAddress | ?{ $_.AddressFamily -eq "IPv4" -and (($_.IPAddress -notmatch "192.") -and ($_.IPAddress -notmatch "10.") -and ($_.IPAddress -notmatch "127."))} 
 
 disable-netadapter -Name $Name.InterfaceAlias -Confirm:$False
+
+$internal = Get-NetIPAddress | ?{ $_.AddressFamily -eq "IPv4" -and (($_.IPAddress -match "192.") -or ($_.IPAddress -match "10.") -and ($_.IPAddress -notmatch "127."))}
+
+netsh interface ipv4 set subinterface $internal.InterfaceAlias mtu=1450 store=persistent
